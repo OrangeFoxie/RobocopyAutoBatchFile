a1054f0ed39f497f06ce370d4da2bc65be45389c
OrangeFoxie <vinhphuc931998@gmai.com>
2021-03-30 11:14:09 +0700

    This is auto file

--- RobocopyAutoScriptBatch.bat
+++ RobocopyAutoScriptBatch.bat
@@ -3,32 +3,51 @@
 set hr=%time:~0,2%
 set mi=%time:~3,2%
 set se=%time:~6,2%
+set time=%hr%h%mi%m%se%s
 
 set dd=%date:~0,2%
 set mo=%date:~3,2%
 set yy=%date:~6,4%
+set date=%dd%-%mo%-%yy%
 
-echo The script's runing right now ;7
-echo Using robocopy which is included in window 10
-echo Today's TIME: %TIME%, DATE: %DATE%
-echo Vui nha :3
+set logName=%date%_(%time%).log
+
+echo --The script's runing right now ;7
+echo --Using robocopy which is included in window 10
+echo --Today's TIME: %TIME%, DATE: %DATE%
+echo --Dinh dang log name be like: %logName%
+echo --Vui nha :3
 
 echo "........................Thong tin copy........................"
 set /P link1=[Thu muc can copy]:
 set /P link2=[Thu muc dich]:
 set /P logLink=[Log luu tai]:
-set logName=%dd%-%mo%-%yy%_(%hr%h%mi%m%se%s).log
+
+	IF /i not exist "%logLink%\CameoAutoRobocopy" (goto state1) else (
+	IF /i exist "%logLink%\CameoAutoRobocopy" (goto state2))
+	
+
+:state1
+		mkdir %logLink%\CameoAutoRobocopy 
+		set logSave=%loglink%\CameoAutoRobocopy\%logName% 
+		echo ------- Dang tao file logs -------
+		echo ------- Magic enhancerrrrrrr :D -------
+		echo ----- The log %logSave% is risingggg -----
+:state2
+		set logSave=%loglink%\CameoAutoRobocopy\%logName%
+		echo ------- Magic has been released, summon the magic report of the highest magical ;D -------
+		echo ------- Log %logSave% has been summoned -------
+
 echo "........................Dang ghi log vao........................"
 
-robocopy %link1% %link2% /E /R:5 /W:1 /MT:8 /log:%loglink%\%logName%
+robocopy %link1% %link2% /E /R:5 /W:1 /MT:8 /log:%logSave%
 
 echo "........................File log da ghi xong........................"
 echo ">>>                             >.<                              <<<"
 echo "........................!!! Copy is done !!!........................"
 echo ">>>                             UwU                              <<<"
+echo "____Thanks to the powerful magic of magicians, the log was saved____"
 pause
-
-
 
 
 ::set link1=C:\Users\Hito\Desktop\1 
